04078caff52f6ba4a71b96024d4fadb6fae917ec
TEO <xogh504@gmail.com>
2021-06-30 00:15:12 +0900

    graphql test

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,38 @@ import Express from 'express'
 
 export const startServer = async () => {
   const app: Express.Application = Express()
+
+
   app.use(Express.json())
-  app.use(Express.urlencoded({extended: false}))
+  app.use(Express.urlencoded({ extended: false }))
 
   const host: string = process.env.HOST || '0.0.0.0'
   const port: string = process.env.PORT || '3000'
+  /**
+   * GraphQL test
+   */
+  const graphqlHTTP = require('express-graphql').graphqlHTTP;
+  const { buildSchema } = require('graphql');
+
+  var schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+  `);
+
+  var hello = {
+    hello: () => {
+      return 'Hello world!';
+    },
+  };
+
+  app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: hello,
+    graphiql: true,
+  }));
+
+
 
   app.get('/', async (req, res) => {
     res.send({ Hello: 'World!' })
