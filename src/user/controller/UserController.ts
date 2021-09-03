6d587d3aae59f36576b33450af4bc0e68054c383
kimjinsoo <1004kjs_@naver.com>
2021-09-03 19:38:24 +0900

    signup부분 수정

--- src/user/controller/UserController.ts
+++ src/user/controller/UserController.ts
@@ -1,22 +1,12 @@
 import express, { NextFunction, Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from 'jsonwebtoken';
-import { check, validationResult } from 'express-validator';
 import { IUser } from "../interfaces/Iuser" 
 import { UserService } from "../services";
 
 // 회원가입
 const signup = async(req: Request, res: Response, next: NextFunction) => {
-    check("id", "id is required").not().isEmpty(),
-    check("password", "password len 6").isLength({min: 8, max: 15})
-    check("nickname", "nickname is required").not().isEmpty(),
-    check("profile", "profile is required").not().isEmpty(),
-    check("sex", "sex is required").not().isEmpty(),
-    check("intro", "intro is required").not().isEmpty(),
-    check('agree_info', "agree_info is required").not().isEmpty(),
-    check('agree_service', "agree_service is required").not().isEmpty(),
-    check('interest', "interest is required").not().isEmpty()
-
+    console.log(11111)
     const { 
         id, 
         password, 
@@ -30,11 +20,6 @@ const signup = async(req: Request, res: Response, next: NextFunction) => {
     }: IUser = req.body;
     
     try {
-        const errors = validationResult(req.body)
-        if(!errors.isEmpty()){
-            return res.status(400).json({ errors: errors.array()})
-        }
-
         const foundUser = await UserService.findId(id);
         if(foundUser) {
             return res.status(400).json({ errors: "해당 id가 이미 존재합니다."})
@@ -42,9 +27,9 @@ const signup = async(req: Request, res: Response, next: NextFunction) => {
 
         const encryptedPassword = bcrypt.hashSync(password, 10)
         const data = {
-            user_id: 2,
+            user_id: 7,
             id: id,
-            password: password,
+            password: encryptedPassword,
             nickname: nickname,
             profile: profile,
             sex: sex,
@@ -56,29 +41,23 @@ const signup = async(req: Request, res: Response, next: NextFunction) => {
         const user = await UserService.createUser(data)
         res.status(201).send({status: "success"})
     } catch(err) {
+        console.log(err)
         next(err)
     }
 }
 
 // 로그인
 const signin = async (req: Request, res: Response, next: NextFunction) => {
-    check("id", "id is required").not().isEmpty()
-    check("password", "password is required").not().isEmpty()
     const { id, password } = req.body;
-    console.log(id, password)
     try {
-        const errors = validationResult(req.body)
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() })
-        }
-
         const check_user = await UserService.findId(id)
+        console.log(check_user?.password)
         if (! check_user) {
             res.status(401).send({
                 errorMessage: "아이디 또는 패스워드가 잘못됐습니다."
             })
         }
-        
+
         const encoded_password = check_user?.password
         const same = bcrypt.compareSync(password, encoded_password)
 
--- src/user/routes/UserRouter.ts
+++ src/user/routes/UserRouter.ts
@@ -1,9 +1,29 @@
 import { Router } from 'express';
 import { UserController }  from '../controller'
-
+import { check, validationResult } from 'express-validator';
 const router = Router();
 
-router.post('/signup', UserController.signup)
+const data_check = [
+    check("id", "id is required").not().isEmpty(),
+    check("password", "password len 6").isLength({ min: 8, max: 15 }),
+    check("nickname", "nickname is required").isEmpty(), 
+    check("profile", "profile is required").not().isEmpty(),
+    check("sex", "sex is required").not().isEmpty(),
+    check("intro", "intro is required").not().isEmpty(),
+    check('agree_info', "agree_info is required").not().isEmpty(),
+    check('agree_service', "agree_service is required").not().isEmpty(),
+    check('interest', "interest is required").not().isEmpty()
+]
+
+const error_callback = (req, res, next) => {
+    const errors = validationResult(req)
+    console.log(errors)
+    if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() })
+    }
+    next()
+}
+router.post('/signup', data_check, error_callback, UserController.signup)
 router.post('/signin', UserController.signin)
 
 export default router
