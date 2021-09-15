0b322798f50b2fde075e071eccf207c6a2ccdbf2
chisacam <dladsds123@gmail.com>
2021-09-15 20:52:35 +0900

    add validation type

--- src/apis/user/routes/UserRouter.ts
+++ src/apis/user/routes/UserRouter.ts
@@ -1,11 +1,11 @@
-import { Router } from 'express'
+import { NextFunction, Request, Response, Router } from 'express'
 import UserController from '../controller'
-import { check, validationResult } from 'express-validator'
+import { check, ValidationChain, validationResult } from 'express-validator'
 import { ValidationFailError } from '../../../lib/customErrorClass'
 import { authToken } from '../../../middleware/tokenVerify'
 const router = Router()
 
-const sign_up_data_check = [
+const sign_up_data_check: ValidationChain[] = [
     check('id', 'id is required').not().isEmpty(),
     check('email', 'email is required').isEmail().not().isEmpty(),
     check('password', 'password len 6').isLength({ min: 8, max: 15 }),
@@ -15,16 +15,16 @@ const sign_up_data_check = [
     check('agree_service', 'agree_service is required').not().isEmpty()
 ]
 
-const sign_in_data_check = [
+const sign_in_data_check: ValidationChain[] = [
     check('email', 'email is required').not().isEmpty(),
     check('password', 'password is required').not().isEmpty(),
 ]
 
-const exist_email_check = [
+const exist_email_check: ValidationChain[] = [
     check('email', 'not valid email').isEmail().not().isEmpty()
 ]
 
-const error_callback = (req, res, next) => {
+const error_callback = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req)
     if (!errors.isEmpty()) throw new ValidationFailError()
     next()
