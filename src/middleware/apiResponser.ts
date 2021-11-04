e09331249d6fcce1e8bf68dfc942e2ce3c6f52d9
chisacam <dladsds123@gmail.com>
2021-11-04 22:49:36 +0900

    no-che header var add

--- src/middleware/apiResponser.ts
+++ src/middleware/apiResponser.ts
@@ -29,7 +29,7 @@ export default ({
     }
 
     res.locals.payload = payload
-    res.set('Cache-Control', 'no-store')
+    res.set('Cache-Control', 'no-cache, no-store, must-revalidate')
     res.status(statusCode).json(payload)
 
     apiResponseLogger({ req, res })
